fb0055404eab42c74c9688be747a63a1fa9a4ef0
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-11 20:16:26 -0500

    adds expressions notes

--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -1,9 +1,28 @@
 package lectures.part1basics
 
-object Expressions extends App{
+object Expressions extends App {
+  //Mathematical expressions
 
   val x = 1 + 2
   // 1+ 2 is an expression
+
+  println(2 + 3 * 4)
+
+  // + - * / & | ^ << >> >>> (right shift with zero extension)
+
+  //relational expressions:
+  println(1 == x)
+  // == != > >= < <=
+
+  println(!(1 == x))
+  // ! && ||
+
+  var aVariable = 2
+  aVariable += 3 // also works with -= *= /= ..... side effects meaning that they only work with a variable
+  println(aVariable)
+
+  // Instructions versus expressions
+
 
 
 }
