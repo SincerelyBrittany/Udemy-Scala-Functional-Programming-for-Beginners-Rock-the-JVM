70a7833c5447f0eedb4934530d20b94d2c147197
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-15 16:25:45 -0400

    adds notes on expressions

--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -37,5 +37,56 @@ object Expressions extends App {
   println(if(aCondition) 5 else 3)
   println(1 + 3)
 
+  var i = 0
+  while (i < 10){
+    println(i)
+    i += 1
+  }
+
+  // NEVER WRITE THIS AGAIN - DONT WRITE IMPERATIVE CODE
+
+  //EVERYTHING IN SCALA IS AN EXPRESSION
+
+  val aWeirdValue = (aVariable = 3) // UnIt === Void
+  println(aWeirdValue)
+  //SIDE EFFECTS IN SCALA ARE ACTUALLY EXPRESSIONS RETURNING UNIT
+  //UNIT === ()
+  // EXAMPLES OF SIDE EFFECTS : println(), whiles, reassigning
+
+  //CODE BLOCKs
+
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+
+  //codeblock is an expression
+  //the value of the block is the value of the last expression
+
+  //val anotherValue = z + 1 // z is inside a codeblock and not available outside.
+
+  // // 1. difference between "hello world" vs println("hello world")?
+
+  //HELLO WORLD IS A LITERAL STRING - A VALUE OF TYPE: STRING. THE PRINTLN AN EXPRESSION WHICH IS A SIDE EFFECT WHICH IS RETURNING UNIT
+  // HAS THE SIDE EFFECT OF PRINTING HELLO WORLD TO THE VALUE
+
+  // 2.
+
+    val someValue = {
+      2 < 3
+    }
+    println(someValue)
+
+  // RETURNS A BOOLEAN
+
+    val someOtherValue = {
+      if(someValue) 239 else 986
+      42
+    }
+    println(someOtherValue)
+
+  // THE VALUE IS 42
 
 }
