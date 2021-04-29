714fffced556b7a138742eb1e9698c6fab199575
SincerelyBrittany <sincerelybr@gmail.com>
2021-04-29 12:14:06 -0400

    creates OObasics object/package

---
+++ src/part2oop/OOBasics.scala
@@ -0,0 +1,26 @@
+package part2oop
+
+object OOBasics extends App{
+
+
+  //instantiate a class by writing "new" Person
+  val person = new Person
+
+}
+
+
+//Class can sit on top level code
+//A Class is like an object constructor, or a "blueprint" for creating objects.
+//A class organizes data and behavior that is code
+
+
+//instantiation means concrete realization and memory that is actual memory speaces that conform to the code
+//and the data structure that the class describes.
+
+/*
+To instantiate is to create such an instance by, for example, defining one particular variation of
+object within a class, giving it a name, and locating it in some physical place. 1) In object-oriented programming,
+some writers say that you instantiate a class to create an object, a concrete instance of the class.
+ */
+
+class Person
