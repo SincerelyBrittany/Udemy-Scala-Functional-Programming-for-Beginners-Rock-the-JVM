692312a3b69a5c8640387697f71da231438f7f2f
Brittany <sincerelybr@gmail.com>
2021-05-07 16:45:33 +0000

    adds Objects.scala

--- src/part2oop/Objects.scala
+++ src/part2oop/Objects.scala
@@ -2,9 +2,12 @@ package part2oop
 
 object Objects extends App{
     //SCALA DOES NOT HAVE CLASS-LEVEL FUNCTIONALITY (STATIC")
-    object Person {
+    object Person { //TYPE + ITS ONLY INSTANCE 
     val N_EYES = 2
     def canFly:Boolean = false
 }
 println(Person.N_EYES)
+
+//SCALA OBJEXT = SINGLETON INSTANCE 
+
 }
